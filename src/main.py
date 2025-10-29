bf15acb8018f7d006725d57d94fcb9ebd29c1ec4
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-10-29 12:37:28 -0600

    Very basic web server

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="src/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request) -> HTMLResponse:
+    """Serve the home page with Hello World message."""
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
